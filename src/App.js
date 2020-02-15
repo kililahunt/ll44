45312a4e6aa8d095fea51762fc7eaea856aa2605
Tuan Huy Nguyen <tuanhuy@Tuans-MacBook-Pro-2.local>
2020-02-16 00:54:05 +0700

    Full Component

--- src/App.js
+++ src/App.js
@@ -1,67 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import TaskForm from './components/TaskForm';
+import Control from './components/Control';
+import TaskList from './components/TaskList';
 
-function App() {
-  return (
-      <div className="container">
+
+class App extends Component {
+	render() {
+		return (
+			<div className="container">
         <div className="text-center">
-            <h1>Work Management</h1>
+            <h1>Task Mamagement</h1>
+            <hr/>
         </div>
         <div className="row">
-           <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-              {/*Form*/}
-              <div className="panel panel-warning">
-                 <div className="panel-heading">
-                    <h3 className="panel-title">
-                        Add work
-                        <span
-                           className="fa fa-times-circle text-right"
-                        ></span>
-                     </h3>
-                 </div>
 
-                 <div className="panel-body">
-                    <form>
-                       <div className="form-group">
-                          <label>Name:</label>
-                          <input 
-                              type="text" 
-                              className="form-control"
-                              name = "name"
-                           />
-                       </div>
+        		{/*FORM*/}
+            <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+             	<TaskForm/>
+            </div>
 
-                       <label>Status:</label>
-                          <select
-                              className = "form-control"
-                              name = "status"
-                           >
-                              <option value = {true}>Active</option>
-                              <option value = {false}>Deactive</option>
+            <div className="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+                <button type="button" className="btn btn-primary">
+                    <span className="fa fa-plus mr-5"></span>Add Task
+                </button>
 
-                          </select><br/>
-                           
-                           <div className="text-center">
-                              <button type="submit" className="btn btn-warning">
-                                 <span className="fa fa-plus mr-5"></span>Save
-                              </button>&nbsp;
-                              <button type="button" className="btn btn-danger">
-                                 <span className="fa fa-close mr-5"></span>Cancel
-                              </button>
-                           </div>
-                  
-                    </form>
-                 </div>
-              </div>
-           </div>
-           <div className="col-xs-8 col-sm-8 col-md-8 col-lg-8">
-              <button type="button" className="btn btn-primary">
-                  <span className="fa fa-plus mr-5"></span>Add
-              </button>
-           </div>
+             {/*SEARCH*/}
+                <div className="row mt-15">
+                    <Control/>
+                </div>
+
+                <div className="row mt-15">
+                    <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                        <TaskList/>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-  );
+    </div>
+	);
+	}
 }
 
 export default App;
